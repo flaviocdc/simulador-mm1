fac13f825e9e32e71b9a182ad587d845f174bcf2
Flávio Coutinho da Costa <flavio.cdc@gmail.com>
2013-06-02 00:48:51 -0400

    Continunando implementacao

--- eventos.py
+++ eventos.py
@@ -1,22 +1,36 @@
 class Evento:
     quando = 0
+    tipo = ''
+    cliente = None
     
-    def __init__(self, quando):
+    def __init__(self, quando, cliente):
+        self.cliente = cliente
         self.quando = quando
-
 
 class EventoChegada(Evento):
     def __str__(self):
-        return 'EventoChegada[quando=%f]' % self.quando
+        return 'EventoChegada[cliente=%s, quando=%f]' % (self.cliente, self.quando)
         
 class EventoSaida(Evento):
     def __str__(self):
-        return 'EventoSaida[quando=%f]' % self.quando
+        return 'EventoSaida[cliente=%s, quando=%f]' % (self.cliente, self.quando)
+
+class EventoAdicionarFila(Evento):    
+    def __str__(self):
+        return 'EventoAdicionarFila[cliente=%s, quando=%f]' % (self.cliente, self.quando)
         
+class EventoSaiuFila(Evento):    
+    def __str__(self):
+        return 'EventoAdicionarFila[cliente=%s, quando=%f]' % (self.cliente, self.quando)
+
 class Cliente:
     chegou = 0
+    atendido = 0
     saiu = 0
     nome = ''
     
     def __init__(self, nome):
         self.nome = 'Cliente-%d' % nome
+    
+    def __str__(self):
+        return self.nome
--- mm1.py
+++ mm1.py
@@ -15,45 +15,93 @@ def tx_chegada(ro, mi):
 tx_saida = 0.1
 txs_chegadas = [ tx_chegada(0.5, tx_saida), tx_chegada(0.7, tx_saida), tx_chegada(0.9, tx_saida) ]
 
-    
 def inserir_ordenado(eventos, evento):
     eventos.append(evento)
     sorted(eventos, key=lambda evt: evt.quando)
 
 
 class Simulador:
-    tempo = 0
+    tempo = 0.0
     eventos = []
     fila = deque()
-    tx_chegada = 0
-    tx_saida = 0
+    tx_chegada = 0.0
+    tx_saida = 0.0
+    total_clientes = 0
+    servidor_ocupado = False
     
     def __init__(self, tx_chegada, tx_saida):
         self.tx_chegada = tx_chegada
         self.tx_saida = tx_saida
         
-    
-    def gerar_proxima_chegada(self):
+    def gerar_proxima_chegada(self, cliente):
         quando = self.tempo + var_exp(self.tx_chegada)
-        return EventoChegada(quando)
+        return EventoChegada(quando, cliente)
     
-    def gerar_proxima_saida(self):
+    def gerar_proxima_saida(self, cliente):
         quando = self.tempo + var_exp(self.tx_saida)
-        return EventoSaida(quando)
+        return EventoSaida(quando, cliente)
+        
+    def criar_novo_cliente(self):
+        self.total_clientes = self.total_clientes + 1
+        return Cliente(self.total_clientes)
+        
+    def imprimir_estado(self):
+        print 'Estado do simulador:'
+        print '- Tempo Atual: %f', self.tempo
+        print '- Total clientes: %d' % self.total_clientes
+        print '- Servidor ocupado: %s' % self.servidor_ocupado
+        print '- Eventos a processar'
+        print map(lambda evt:'%s' % str(evt), self.eventos)
+        print '- Fila'
+        print map(lambda client:'%s' % str(client), self.fila)
     
     def simular(self):
-        inserir_ordenado(self.eventos, self.gerar_proxima_chegada())
+        print 'Iniciando simulacao'
+        
+        cliente = self.criar_novo_cliente()
+        inserir_ordenado(self.eventos, self.gerar_proxima_chegada(cliente))
         
         while (True):
-            # TODO remover dps
-            if len(self.eventos) == 0:
-                break
-            
             evento = self.eventos.pop(0)
             
-            if isinstance(evento, EventoChegada):
+            if isinstance(evento, EventoChegada):        
+                # quem chegou
+                cliente = evento.cliente
+                cliente.chegou = self.tempo
+                
+                # inserindo cliente na fila
+                self.fila.append(cliente)
+                
+                # avancando o tempo
+                self.tempo = evento.quando
+                
+                print 'Alguem chegou  %s as %f' % (cliente, evento.quando)
+                
                 # gerando a proxima chegada
-                inserir_ordenado(self.eventos, self.gerar_proxima_chegada())
-        
+                inserir_ordenado(self.eventos, self.gerar_proxima_chegada(self.criar_novo_cliente()))
+                
+            elif isinstance(evento, EventoSaida):
+                # quem esta saindo
+                evento.cliente.saiu = self.tempo
+                
+                # servidor nao esta mais ocupado
+                self.servidor_ocupado = False
+                
+                print 'Alguem saiu  %s as %f' % (cliente, evento.quando)
+            
+            if len(self.fila) != 0 and not self.servidor_ocupado:
+                cliente = self.fila.popleft()
+                
+                print 'Servidor atendendo a %s as %f' % (cliente, self.tempo)
+                
+                cliente.atendido = self.tempo
+                inserir_ordenado(self.eventos, self.gerar_proxima_saida(cliente))
+            
+            self.imprimir_estado()
+                        
+            try:
+                raw_input('Qualquer tecla para a proxima iteracao...')
+            except exceptions.EOFError:
+                pass
         
 Simulador(txs_chegadas[0], tx_saida).simular()
