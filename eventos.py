9ef58b2e26656837c3c7de167c37be667f198612
Flávio Coutinho da Costa <flavio.cdc@gmail.com>
2013-06-01 21:37:32 -0400

    Refatorando o codigo do simulador para uma classe

--- eventos.py
+++ eventos.py
@@ -1,17 +1,22 @@
-class EventoChegada:
+class Evento:
     quando = 0
     
     def __init__(self, quando):
         self.quando = quando
-    
+
+
+class EventoChegada(Evento):
     def __str__(self):
         return 'EventoChegada[quando=%f]' % self.quando
         
-class EventoSaida:
-    quando = 0
-    
-    def __init__(self, quando):
-        self.quando = quando
-    
+class EventoSaida(Evento):
     def __str__(self):
         return 'EventoSaida[quando=%f]' % self.quando
+        
+class Cliente:
+    chegou = 0
+    saiu = 0
+    nome = ''
+    
+    def __init__(self, nome):
+        self.nome = 'Cliente-%d' % nome
--- mm1.py
+++ mm1.py
@@ -15,38 +15,45 @@ def tx_chegada(ro, mi):
 tx_saida = 0.1
 txs_chegadas = [ tx_chegada(0.5, tx_saida), tx_chegada(0.7, tx_saida), tx_chegada(0.9, tx_saida) ]
 
-def gerar_proxima_chegada(tx_chegada, tempo):
-    quando = tempo + var_exp(tx_chegada)
-    return EventoChegada(quando)
-    
-def gerar_proxima_saida(tx_saida, tempo):
-    quando = tempo + var_exp(tx_chegada)
-    return EventoSaida(quando)
     
 def inserir_ordenado(eventos, evento):
     eventos.append(evento)
     sorted(eventos, key=lambda evt: evt.quando)
 
-def simular(tx_chegada, tx_saida):
+
+class Simulador:
     tempo = 0
     eventos = []
+    fila = deque()
+    tx_chegada = 0
+    tx_saida = 0
     
-    inserir_ordenado(eventos, gerar_proxima_chegada(tx_chegada, tempo))
+    def __init__(self, tx_chegada, tx_saida):
+        self.tx_chegada = tx_chegada
+        self.tx_saida = tx_saida
+        
     
-    while (True):
-        if len(eventos) == 0:
-            break
+    def gerar_proxima_chegada(self):
+        quando = self.tempo + var_exp(self.tx_chegada)
+        return EventoChegada(quando)
+    
+    def gerar_proxima_saida(self):
+        quando = self.tempo + var_exp(self.tx_saida)
+        return EventoSaida(quando)
+    
+    def simular(self):
+        inserir_ordenado(self.eventos, self.gerar_proxima_chegada())
         
-        evento = eventos.pop(0)
-        
-        if isinstance(evento, EventoChegada):
-            # gerando a proxima chegada
-            inserir_ordenado(eventos, gerar_proxima_chegada(tx_chegada, tempo))
+        while (True):
+            # TODO remover dps
+            if len(self.eventos) == 0:
+                break
             
+            evento = self.eventos.pop(0)
             
-            
-
+            if isinstance(evento, EventoChegada):
+                # gerando a proxima chegada
+                inserir_ordenado(self.eventos, self.gerar_proxima_chegada())
         
         
-
-simular(txs_chegadas[0], tx_saida)
+Simulador(txs_chegadas[0], tx_saida).simular()
